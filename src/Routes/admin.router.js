bf7389262710df534cdd7d528a9dcc83759208f2
Gaurav Kandhalia <vaidkandhala333@gmail.com>
2025-09-22 13:19:31 +0530

    backend completed

--- src/Routes/admin.router.js
+++ src/Routes/admin.router.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { isAuthenticated } from "../Middlewares/auth.middleware.js";
 import { login } from "../Controllers/auth.controller.js";
-import { createAgent, distributeController } from "../Controllers/admin.controller.js";
+import { createAgent, distributeController, getDistributedItems } from "../Controllers/admin.controller.js";
 import { upload } from "../Middlewares/multer.middleware.js";
 import { uploadFile } from "../Controllers/admin.controller.js";
 const adminRouter = Router();
@@ -10,5 +10,6 @@ adminRouter.post("/login",login);
 adminRouter.post("/create-agent",isAuthenticated,createAgent);
 adminRouter.post("/uploadFile",isAuthenticated,upload.single("file"),uploadFile);
 adminRouter.post("/distributeItems",isAuthenticated,distributeController);
+adminRouter.get("/Items",isAuthenticated,getDistributedItems)
 
 export default adminRouter;
